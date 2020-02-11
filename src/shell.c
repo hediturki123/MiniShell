bfa9e70339a235926a5a30d487f264ec7af7e3ec
hediturki123 <hedi.turki.sanekli@gmail.com>
2020-02-11 15:14:08 +0100

    gestion des erreurs done

--- src/shell.c
+++ src/shell.c
@@ -25,49 +25,55 @@ int main()
 			printf("exit\n");
 			exit(0);
 		}
-
-		if (l->err) {
+		if (l->err){
 			/* Syntax error, read another command */
-			printf("error: %s\n", l->err);
-			continue;
+						printf("error:\n");
+						continue;
 		}
-
-		
 		
 
 		/* Display each command of the pipe */
 		for (i=0; l->seq[i]!=0; i++) {
 			char **cmd = l->seq[i];
-			if (strcmp(cmd[0],"quit")==0){
+			for (j=0; cmd[j]!=0; j++) {
+				//printf("%s ", cmd[j]);
+				if (strcmp(cmd[0],"quit")==0){
 					exit(0);
-			} else {
-				//printf("seq[%d]: ", i);
-				for (j=0; cmd[j]!=0; j++) {
-					//printf("%s ", cmd[j]);
-					pid=fork();
-    				if(pid==0){
-						
-						if (l->in){
-							printf("in: %s\n", l->in);
-							int in=open(l->in,O_RDONLY| O_CREAT,0666);
-							Dup2(in,0);
-							printf("\n");
+				}
+				pid=fork();
+    			if(pid==0){
+					//printf("cmd= %s ", l->seq[i][0]);
+					if (l->in){
+						printf("in: %s\n", l->in);
+						int in=open(l->in,O_RDONLY);
+						if (in<0){
+							Dup2(1,2);
+							printf("%s: Permission denied\n",l->in);
+							exit(0);
 						}
-
-						if (l->out) {
-							printf("out: %s\n", l->out);
-							int out=open(l->out,O_WRONLY| O_CREAT,0666);
-							Dup2(out,1);
-						}
-
-						execvp(cmd[0],cmd);
-					} else {
-						if(waitpid(pid,&status,0)==-1){
-							printf("error");
-							exit(-1);
-						}
+						Dup2(in,0);
 					}
-					
+
+					if (l->out) {
+						printf("out: %s\n", l->out);
+						int out=open(l->out,O_WRONLY| O_CREAT,0666);
+						if (out<0){
+							Dup2(1,2);
+							printf("%s: Permission denied\n",l->out);
+							exit(0);
+						}
+						Dup2(out,1);
+					}
+					if (execvp(cmd[0],cmd)==-1) { //remplace le l->err
+						/* Syntax error, read another command */
+						printf("%s: command not found\n", cmd[0]);
+					}
+					exit(0);
+				} else {
+					if(waitpid(pid,&status,0)==-1){
+						printf("error\n");
+						exit(-1);
+					}
 				}
 			}
 		}
