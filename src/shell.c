786ea13447266b5b1724f0e75e156e2709b1abc1
hediturki123 <hedi.turki.sanekli@gmail.com>
2020-02-11 17:05:21 +0100

    faire pipe

--- src/shell.c
+++ src/shell.c
@@ -19,7 +19,7 @@ int main()
 {
 	while (1) {
 		struct cmdline *l;
-		int i, j;
+		int i;
 		pid_t pid;
 		int status;
 		int tube[2];
@@ -32,57 +32,47 @@ int main()
 			printf("exit\n");
 			exit(0);
 		}
-		if (l->err){
-			/* Syntax error, read another command */
-						printf("error:\n");
-						continue;
-		}
 		
-		if (pipe(tube) == -1) {
-			printf("error tube!!");
-			exit(1);
+		if (pipe(tube) == -1){
+			perror("pipe error");
 		}
+
 		/* Display each command of the pipe */
 		for (i=0; l->seq[i]!=0; i++) {
 			char **cmd = l->seq[i];
-			for (j=0; cmd[j]!=0; j++) {
+			if (strcmp(cmd[0],"quit")==0){
+				exit(0);
+			}
+			pid=fork();
+    		if(pid==0){
 				//printf("cmd= %s ", l->seq[i][0]);
-				//printf("%s ", cmd[j]);
-				if (strcmp(cmd[0],"quit")==0){
-					exit(0);
+				if (l->in){
+					//printf("in: %s\n", l->in);
+					int in=open(l->in,O_RDONLY);
+					if (in<0){
+						perror(l->in); //affiche le message d'erreur
+						exit(0);
+					}
+					Dup2(in,0);
 				}
-				pid=fork();
-    			if(pid==0){
-					//printf("cmd= %s ", l->seq[i][0]);
-					if (l->in){
-						//printf("in: %s\n", l->in);
-						int in=open(l->in,O_RDONLY);
-						if (in<0){
-							perror(l->in); //affiche le message d'erreur
-							exit(0);
-						}
-						Dup2(in,0);
+				if (l->out) {
+					//printf("out: %s\n", l->out);
+					int out=open(l->out,O_WRONLY| O_CREAT,0666);
+					if (out<0){
+						perror(l->out);
+						exit(0);
 					}
-					if (l->out) {
-						//printf("out: %s\n", l->out);
-						int out=open(l->out,O_WRONLY| O_CREAT,0666);
-						if (out<0){
-							perror(l->out);
-							exit(0);
-						}
-						Dup2(out,1);
-					}
-					if (execvp(cmd[0],cmd)==-1) { //remplace le l->err
-						/* Syntax error, read another command */
-						//printf("******* %s *****",cmd[0]);
-						perror(cmd[0]);
-					}
-					exit(0);
-				} else {
-					if(waitpid(pid,&status,0)==-1){
-						printf("error\n");
-						exit(-1);
-					}
+					Dup2(out,1);
+				}
+				if (execvp(cmd[0],cmd)==-1) { //remplace le l->err
+					/* Syntax error, read another command */
+					perror(cmd[0]);
+				}
+				exit(0);
+			} else {
+				if(waitpid(pid,&status,0)==-1){
+					printf("error\n");
+					exit(-1);
 				}
 			}
 		}
