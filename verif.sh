b5524c24a1c85212dfa5319a5d8bc8b7313dc3ed
DOLIPRANE74 <DOLIPRANE74@gitub.com>
2020-02-12 21:26:14 +0100

    MAJ du script

--- verif.sh
+++ verif.sh
@@ -1,3 +1,7 @@
+############################################################
+#	Attendu le mini shell ne prend pas en compte les "
+############################################################
+
 make clean
 make
 for i in $(seq 1)
@@ -6,11 +10,8 @@ do
 	echo "Test du fichier test$i.txt"
 	resultat=$(./sdriver.pl -t tests/test$i.txt -s ./shell | head -n -1 | sed "1,3d" | tr -s " ")
 	res_attendu=$(cat tests/testres$i.txt | tr -s " ")
-	echo "**********"
-	echo "$resultat"
-	echo "**********"
-	echo "$res_attendu"
-	if [ resultat != res_attendu ]
+
+	if [[ $resultat != $res_attendu ]]
 	then
 		echo "Test faux"
 	else
